30029b1e7c5ba18c1e9b9dd62d617f86c16f283b
neojinya <wonder.yes.world@gmail.com>
2019-10-18 20:24:27 +0900

    変更しました

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,28 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+lock '3.11.2'
+
+set :application, 'chat-space'
+
+set :repo_url,  'git@github.com:neojinya/chat-space.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/neojinya.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
